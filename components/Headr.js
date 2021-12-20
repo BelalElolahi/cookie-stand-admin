42c1a4b060dce1776519d2e723516d065dd53dfc
BelalElolhi <belalelolahi003@gmail.com>
2021-12-20 18:33:30 +0200

    cookie stand admin next.js

---
+++ components/Headr.js
@@ -0,0 +1,9 @@
+export default function Header (){
+    return(
+        <header className="flex justify-between -mt-5 bg-green-600 py-7">
+        <h1 className="ml-6 text-5xl font-bold font-weight: 900">Cookie Stand Admin</h1>
+       
+         </header>
+        
+        )
+}
---
+++ components/footer.js
@@ -0,0 +1,11 @@
+export default function Footer(){
+    return (
+        <footer className="flex justify-between mb-5 -mt-2 bg-green-600 py-7">
+            <h1 className="ml-6 text-2xl font-weight: 900">
+                @2021
+            </h1>
+        </footer>
+    )
+
+
+}
